9f77e4d47dfc1b9a3fdb4dddb687020458ee8424
Bekzod <bekzodjonxasanov7@gmail.com>
2026-01-06 14:00:14 +0500

    added

---
+++ dars1/dars1/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace dars1;
+
+public class Product
+{
+    public string name { get; set; }
+    public int price { get; set; }
+    public Guid productId { get; set; }
+    public DateTime period { get; set; }
+    public DateTime endperiod { get; set; }
+
+    override public string ToString()
+    {
+        return $" ProductId: {productId}\n Product: {name}\n Price: {price}\n Period: {period}\n EndPeriod: {endperiod}";
+    }
+}
---
+++ dars1/dars1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace dars1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Product product = new Product
+        {
+            name = "Apple Juce",
+            price = 100,
+            productId = Guid.NewGuid(),
+            period = DateTime.Now,
+            endperiod = DateTime.Now.AddMonths(1)
+        };
+        Product product2 = new Product
+        {
+            name = "Milk",
+            price = 100,
+            productId = Guid.NewGuid(),
+            period = DateTime.Now,
+            endperiod = DateTime.Now.AddMonths(2)
+        };
+        Console.WriteLine();
+        Console.WriteLine(product);
+        Console.WriteLine();
+        Console.WriteLine(product2);
+    }
+}
