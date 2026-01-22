dc54bd2ec239c489b43dc2155d603cf22b6c8722
Bekzod <bekzodjonxasanov7@gmail.com>
2026-01-22 14:12:44 +0500

    added

---
+++ dars7/dars7/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace dars7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            user.UserId = Guid.NewGuid();
+            user.UserName = "johndoe";
+            user.Password = "12345";
+            user.FirstName = "John";
+            user.LastName = "Doe";
+            Console.WriteLine($"{user.FirstName} {user.LastName}  {user.UserName}, ID: {user.UserId}");
+        }
+    }
+}
---
+++ dars7/dars7/User.cs
@@ -0,0 +1,44 @@
+﻿namespace dars7;
+
+public class User
+{
+    private Guid userId;
+
+    public Guid UserId
+    {
+        get { return userId; }
+        set { userId = value; }
+    }
+
+    private string userName;
+
+    public string UserName
+    {
+        get { return userName; }
+        set { userName = value; }
+    }
+
+    private string password;
+
+    public string Password
+    {
+        get { return password; }
+        set { password = value; }
+    }
+    private string lastName;
+
+    public string LastName
+    {
+        get { return lastName; }
+        set { lastName = value; }
+    }
+
+    private string firstName;
+
+    public string FirstName
+    {
+        get { return firstName; }
+        set { firstName = value; }
+    }
+
+}
